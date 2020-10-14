627c3be5721de803a4c9d73de1d038df877df00c
おうこう <190229ir@yse-c.net>
2020-10-14 10:05:18 +0900

    login.php

--- login.php
+++ login.php
@@ -14,60 +14,57 @@
 //⑥セッションを開始する
 
 //①名前とパスワードを入れる変数を初期化する
-$name = '';
-$password = '';
-
+$name="";
+$pass="";
 /*
  * ②ログインボタンが押されたかを判定する。
  * 押されていた場合はif文の中の処理を行う
  */
-if (isset($_POST['decision'])) {
-	/*
-	 * ③名前とパスワードが両方とも入力されているかを判定する。
-	 * 入力されていた場合はif文の中の処理を行う。
-	 */
-	if (!empty($_POST['name']) && !empty($_POST['pass'])) {
-		//④名前とパスワードにPOSTで送られてきた名前とパスワードを設定する
-		$name = $_POST['name'];
-		$password = $_POST['pass'];
-	} else {
-		//⑤名前かパスワードが入力されていない場合は、「名前かパスワードが未入力です」という文言をメッセージを入れる変数に設定する
-		$error = '名前かパスワードが未入力です';
-	}
-}
 
-//⑦名前が入力されているか判定する。入力されていた場合はif文の中に入る
-if (/* ⑦の処理を書く */) {
-	//⑧名前に「yse」、パスワードに「2019」と設定されているか確認する。設定されていた場合はif文の中に入る
-	if (/* ⑧の処理を書く */){
-		//⑨SESSIONに名前を設定し、SESSIONの「login」フラグをtrueにする
-		//⑩在庫一覧画面へ遷移する
-		header(/* ⑩の遷移先を書く */);
-	}else{
-		//⑪名前もしくはパスワードが間違っていた場合は、「ユーザー名かパスワードが間違っています」という文言をメッセージを入れる変数に設定する
-	}
-}
+// if (isset($_POST["decision"])) {
+// 	/*
+// 	 * ③名前とパスワードが両方とも入力されているかを判定する。
+// 	 * 入力されていた場合はif文の中の処理を行う。
+// 	 */
+// 	if (/* ③の処理を書く */) {
+// 		//④名前とパスワードにPOSTで送られてきた名前とパスワードを設定する
+// 	} else {
+// 		//⑤名前かパスワードが入力されていない場合は、「名前かパスワードが未入力です」という文言をメッセージを入れる変数に設定する
+// 	}
+// }
 
-//⑫SESSIONの「error2」に値が入っているか判定する。入っていた場合はif文の中に入る
-if (/* ⑫の処理を書く */) {
-	//⑬SESSIONの「error2」の値をエラーメッセージを入れる変数に設定する。
-	//⑭SESSIONの「error2」にnullを入れる。
-}
+// //⑦名前が入力されているか判定する。入力されていた場合はif文の中に入る
+// if (/* ⑦の処理を書く */) {
+// 	//⑧名前に「yse」、パスワードに「2019」と設定されているか確認する。設定されていた場合はif文の中に入る
+// 	if (/* ⑧の処理を書く */){
+// 		//⑨SESSIONに名前を設定し、SESSIONの「login」フラグをtrueにする
+// 		//⑩在庫一覧画面へ遷移する
+// 		header(/* ⑩の遷移先を書く */);
+// 	}else{
+// 		//⑪名前もしくはパスワードが間違っていた場合は、「ユーザー名かパスワードが間違っています」という文言をメッセージを入れる変数に設定する
+// 	}
+// }
+
+// //⑫SESSIONの「error2」に値が入っているか判定する。入っていた場合はif文の中に入る
+// if (/* ⑫の処理を書く */) {
+// 	//⑬SESSIONの「error2」の値をエラーメッセージを入れる変数に設定する。
+// 	//⑭SESSIONの「error2」にnullを入れる。
+// }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<title>ログイン</title>
-	<link rel="stylesheet" href="css/login.css" type="text/css" />
+<meta charset="UTF-8">
+<title>ログイン</title>
+<link rel="stylesheet" href="css/login.css" type="text/css" />
 </head>
 <body id="login">
 	<div id="main">
 		<h1>ログイン</h1>
 		<?php
-		//⑮エラーメッセージの変数に入っている値を表示する
+		// //⑮エラーメッセージの変数に入っている値を表示する
 		// echo "<div id='error'>", /* ⑮の変数を書く */, "</div>";
-
+		
 		// //⑯メッセージの変数に入っている値を表示する
 		// echo "<div id='msg'>", /* ⑯の変数を書く */, "</div>";
 		?>
@@ -76,13 +73,14 @@ if (/* ⑫の処理を書く */) {
 				<input type='text' name="name" size='5' placeholder="Username">
 			</p>
 			<p>
-				<input type='password' name='pass' size='5' maxlength='20' placeholder="Password">
+				<input type='password' name='pass' size='5' maxlength='20'
+					placeholder="Password">
 			</p>
 			<p>
-				<button type="submit" formmethod="POST" name="decision" value="1" id="button">Login</button>
+				<button type="submit" formmethod="POST" name="decision" value="1"
+					id="button">Login</button>
 			</p>
 		</form>
 	</div>
 </body>
-
 </html>
